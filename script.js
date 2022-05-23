af4c6d9ef4e90d626e197df078c781935017f39e
Gcoder9 <104526581+Gcoder9@users.noreply.github.com>
2022-05-23 16:03:39 -0400

    start

--- script.js
+++ script.js
@@ -1,5 +1,28 @@
-// Assignment Code
+
+    
+      // Assignment Code
 var generateBtn = document.querySelector("#generate");
+
+// Instructor Provided Template: Anthony Cooper
+function generatePassword() {
+  var lowercase = "abcdefghijklmnopqrstuvwxyz";
+  var numbers = "0123456789";
+  var special = "#$%&()*+,-./:;<=>?";
+  var uppercase = lowercase.toUpperCase();
+
+  var input = parseInt(prompt("Choose a lenght between 8 and 128"));
+
+  if (input >= 8 && input <= 128) {
+    // GOOD CODE GOES HERE
+  } else {
+    alert("Enter a valid number");
+  }
+
+  return "";
+
+
+}
+
 
 // Write password to the #password input
 function writePassword() {
@@ -10,5 +33,15 @@ function writePassword() {
 
 }
 
-// Add event listener to generate button
+ var generateBtn = document.querySelector("#generate");
+  
+
+  function writePassword() {
+  var password = generatePassword();
+  var passwordText = document.querySelector("#password");
+
+  passwordText.value = password;
+
+}
+
 generateBtn.addEventListener("click", writePassword);
