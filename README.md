4db1e2e617eb14549b69b4a907c53b1e9ee156d0
Gcoder9 <104526581+Gcoder9@users.noreply.github.com>
2022-05-24 16:40:46 -0400

    readme

--- README.md
+++ README.md
@@ -1,1 +1,4 @@
 # passwordgenerator
+<p>This is a password generator. The length of characters should be between 8 to 128. It should ask you wether to include lowercase, uppercase, special characters, and numbers. It will be validated when you choose at least one character. THEN a password is generated that matches the selected criteria.</p>
+
+<img src="https://www.awesomescreenshot.com/image/27658438?key=e5d46b5ab66e739ee3653fdd1b6b7c14">
--- script.js
+++ script.js
@@ -10,18 +10,18 @@ function generatePassword() {
   var special = "#$%&()*+,-./:;<=>?";
   var uppercase = lowercase.toUpperCase();
 
-  var input = parseInt(prompt("Choose a lenght between 8 and 128"));
+  var input = prompt("Choose a length between 8 and 128");
 
   if (input >= 8 && input <= 128) {
     // GOOD CODE GOES HERE
   } else {
     alert("Enter a valid number");
-    return generatePassword();
+    return generatePassword();  
 
   }
 
   if (input >= 8 && input <= 128) {
-    var special = confirm('Include speacial characters?');
+    var special = confirm('Include special characters?');
     var numbers = confirm('Include numbers?');
     var lowercase = confirm('Include lowercase characters?');
     var uppercase = confirm('Include uppercase characters');
@@ -30,31 +30,50 @@ function generatePassword() {
 if (special === false && numbers === false && lowercase === false && uppercase === false){
   alert('Please select your criteria!');
   return generatePassword();
+}  
+
+if (special) {
+  var special = ["!@#$%^&*()"];
 }
-
-
+if (special===false) {
+  var special = [""];
 }
+if (numbers) {
+  var numbers = ["0123456789"]
+}
+if (numbers===false) {
+  var numbers = [""]
+}
+if (lowercase) {
+  var lowercase = ["abcdefghijklmnopqrstuvwxyz"]
+}
+if (lowercase===false) {
+  var lowercase = [""]
+}
+if (uppercase) {
+  var uppercase = ["ABCDEFGHIJKLMNOPQRSTUVWXYZ"]
+}
+if (uppercase===false) {
+  var uppercase = [""]
+}
+var randomChar = special + numbers + lowercase + uppercase
 
-
-
+var password = "";
+for (var i =0; i < input; i++) {
+  var randomInput = randomChar[Math.floor(Math.random() * randomChar.length)];
+  password += randomInput;
+  
+}
+return password;
+}
 // Write password to the #password input
 function writePassword() {
   var password = generatePassword();
   var passwordText = document.querySelector("#password");
 
   passwordText.value = password;
-
+  
 }
 
  var generateBtn = document.querySelector("#generate");
-  
-
-  function writePassword() {
-  var password = generatePassword();
-  var passwordText = document.querySelector("#password");
-
-  passwordText.value = password;
-
-}
-
 generateBtn.addEventListener("click", writePassword);
